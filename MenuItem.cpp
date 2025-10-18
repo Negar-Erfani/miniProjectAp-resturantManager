06e0ef6277b7327f4c95f16fd9e46b3b56185a9f
Negar-Erfani <negarer1384@gmail.com>
2025-10-18 10:14:25 +0330

    updating menu item file features

--- MenuItem.cpp
+++ MenuItem.cpp
@@ -13,17 +13,19 @@ MenuItem::MenuItem (string name, Menu* menu) : name(name)
 {
     string fileName = name+".txt";
     food = new linkedlist();
-    fstream file(fileName, ios::in | ios::out | ios::app);
+    ifstream file(fileName);
     string temp;
 
     
         if (!file.is_open())
         {
-            file.open(fileName, ios::out);
+            file.close();
+            ofstream newFile;
+            newFile.open(fileName);
             string userDefinedFood;
             cout << "-----------------------------------------------------------"<<endl;
             cout << "\033[31m"<< "please enter your list for "<< name << ". " << endl;
-            cout << "write" << "\033[1m" << "'end' " << "\033[22m" << "when you are done" << "\033[0m :" <<endl;
+            cout << "write" << "\033[1m " << "end " << "\033[22m" << "when you are done" << "\033[0m :" <<endl;
             while(cin >> userDefinedFood)
             {
                 if(userDefinedFood == "end")
@@ -31,24 +33,22 @@ MenuItem::MenuItem (string name, Menu* menu) : name(name)
                     #ifdef _WIN32
                     system("chcp 65001"); // تنظیم کدپیج UTF-8 در ویندوز
                     #endif
-                    cout << "Added successfully \U0001F600" << endl;
+                    cout << "\U0001F600"<< "\033[32m" << " Added successfully" <<"\033[0m"<< endl;
                     break;
                 }
                 else
                 {
-                    file << userDefinedFood << endl;
+                    newFile << userDefinedFood << endl;
                 }
             }
-            file.close();
         }
 
     
-    file.open(fileName, ios::in | ios::out);
-    file.seekp(0, ios::end);
-    string temp;
-    while (file >> temp) 
+    file.open(fileName);
+    string temp2;
+    while (file >> temp2) 
     {
-        food->insert(temp);
+        food->insert(temp2);
     }
     file.close();
 }
